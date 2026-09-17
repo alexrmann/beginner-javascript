bc33388b8fc97891df9f6dc10bd7a5d9451932b1
Alex R. Mann <33494521+alexrmann@users.noreply.github.com>
2026-09-16 17:49:36 -0700

    Add example code to demonstrate how ESLint works

---
+++ playground/my-work/some.js
@@ -0,0 +1,13 @@
+const p = document.querySelector('p');
+console.log('im in another file');
+
+const young = 100;
+// console.log(p, young);
+
+function hi() {
+  const age = 200;
+  // return age;
+}
+
+// hi();
+console.log();
